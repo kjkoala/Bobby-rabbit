d2a22db26ddf4732f205eca7096c87a9d8599c0c
Matvienko Igor <pheonixicq@gmail.com>
2023-12-07 02:32:12 +0500

    main

--- src/common/VKBridge.ts
+++ src/common/VKBridge.ts
@@ -7,7 +7,7 @@ class VK {
   private prev: number;
   constructor() {
     this.countLevels = 0;
-    this.whenShowAds = 6;
+    this.whenShowAds = 5;
     this.prev = this.whenShowAds - 1;
   }
     static init() {
