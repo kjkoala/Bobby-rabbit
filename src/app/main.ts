f475aa6921515d931a7e998286ea5be2b25a61e4
Matvienko Igor <pheonixicq@gmail.com>
2024-02-26 23:12:24 +0500

    added custom loader

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,12 +1,12 @@
-import { Color, Engine, Loader, DisplayMode } from "excalibur";
+import { Color, Engine, Loader, DisplayMode, type Loadable } from "excalibur";
 import { TiledMapResource } from '@excaliburjs/plugin-tiled';
 import './style.css';
 import { eggs_levels, levels } from "./levels";
 import { resources } from "./resources";
 import { Menu } from "src/scenes/mainMenu";
 import { isMobile, tileFinish, tilemap } from "src/common/constants";
-import { bobbyCarrotLogo } from "./bobbyCarrot";
 import VKBridge from "src/common/VKBridge";
+import LoaderUI from 'src/ui/Loader.svelte'
 
 const convertPath = (map: TiledMapResource) => {
   map.convertPath = (_originPath: string, relativePath: string): string => {
@@ -18,6 +18,34 @@ const convertPath = (map: TiledMapResource) => {
     }
     return 'levels/'+relativePath
   }
+}
+
+
+export class CustomLoader extends Loader {
+  loaderUI!: LoaderUI
+  constructor(loadables?: Loadable<any>[]) {
+    super(loadables)
+
+    this.playButtonText = "Запустить игру";
+  }
+
+  wireEngine(engine: Engine): void {
+    this.loaderUI = new LoaderUI({
+      target: document.querySelector('#root')!,
+    })
+
+    super.wireEngine(engine)
+  }
+
+  destroyUI() {
+    this.loaderUI.$destroy()
+  }
+  draw(): void {
+    this.loaderUI.$set({
+      loader: this.progress
+    })
+  }
+  
 }
 
 
@@ -34,6 +62,7 @@ const engine = new Engine({
   maxFps: 60,
   backgroundColor: Color.Black,
 })
+
 export const carrotsMaps = levels.map(level => new TiledMapResource(level))
 export const eggsMaps = eggs_levels.map(level => new TiledMapResource(level))
 engine.addScene('menu', new Menu)
@@ -42,17 +71,12 @@ eggsMaps.forEach(convertPath)
 
 carrotsMaps.forEach(convertPath)
 
-const loader = new Loader([...Object.values(resources), ...carrotsMaps, ...eggsMaps])
+const loader = new CustomLoader([...Object.values(resources), ...carrotsMaps, ...eggsMaps])
 
-
-loader.logoWidth = 186;
-loader.logoHeight = 168;
-loader.logo = bobbyCarrotLogo
-
-loader.playButtonText = "Запустить игру";
 engine.start(loader).then(() => {
     // Баг движка, если изменится размер экрана то при загрузке сцены экран не обновится
     window.dispatchEvent(new Event('resize'));
+    loader.destroyUI()
     engine.goToScene('menu')
     VKBridge.checkAds()
 });
